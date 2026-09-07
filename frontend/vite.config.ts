888a729962f898d40b230e21e0049ceaa971bff4
Alya Campbell <94196424+alyarshelle@users.noreply.github.com>
2026-09-07 01:45:53 -0400

    Set base path for Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/gamegrid/',
   plugins: [react()],
   server: {
     proxy: {
