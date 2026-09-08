e8281890647038519374c4764dc529d1a8324a94
alyarshelle <alya.arshelle@gmail.com>
2026-09-08 19:40:37 -0400

    working on github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,19 +10,27 @@
 //   ],
 // })
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/gamegrid/',
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://ncaa-api.henrygd.me',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/gamegrid/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ncaa-api.henrygd.me',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
