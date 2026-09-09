9f17ec4b15f7baec8f9d96cad0ea0aed074fcd7f
alyarshelle <alya.arshelle@gmail.com>
2026-09-08 22:49:04 -0400

    using hashrouter for reloading

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import MainPage from './pages/MainPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/gamegrid">
+    <HashRouter basename="/gamegrid">
       <MainPage />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
